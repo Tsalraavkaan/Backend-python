0b5f7b78a087ed4646f06b61a384965276b6cd0e
Andrej <72415904+Tsalraavkaan@users.noreply.github.com>
2021-10-22 15:00:49 +0000

    Final version
    Доделан метакласс вместе с тестами

--- Task-2/metaclass.py
+++ Task-2/metaclass.py
@@ -1,26 +1,17 @@
 class CustomMeta(type):
-    def __new__(metaclass, future_class_name, 
+    def __new__(metaclass, future_class_name,
                 future_class_parents, future_class_attr):
-        attrs = ((name, value) for name, value in future_class_attr.items() if not name.startswith('__'))
-        uppercase_attr = dict(("custom_" + name, value) for name, value in attrs)
-        uppercase_attr.append("custom_" + attr.__name__, attr)
-        return type.__new__(metaclass, future_class_name, 
-                            future_class_parents, uppercase_attr)
+        attrs = ((name, value) for name, value in future_class_attr.items()
+                 if not name.startswith('__'))
+        new_attrs = dict(("custom_" + name, value) for name, value in attrs)
+        for name, value in future_class_attr.items():
+            if name.startswith('__'):
+                new_attrs[name] = value
+        new_attrs["__setattr__"] = CustomMeta.setattr
+        return type.__new__(metaclass, future_class_name,
+                            future_class_parents, new_attrs)
 
-class CustomClass(metaclass=CustomMeta):
-    x = 50
-
-    def __init__(self, val=99):
-        self.val = val
-
-    def line(self):
-        return 100
-
-inst = CustomClass()
-inst.custom_x
-inst.custom_line()
-inst.custom_val
-
-inst.x  # ошибка
-inst.val  # ошибка
-inst.line() # ошибка
+    def setattr(self, name, value):
+        if name not in self.__dict__:
+            name = "custom_" + name
+        self.__dict__[name] = value
