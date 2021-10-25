2802bdefeaed57943c637f23f97b02227f230607
Andrej <72415904+Tsalraavkaan@users.noreply.github.com>
2021-10-25 11:34:46 +0000

    First version
    Сделаны конфиг и app

---
+++ nginx/app.py
@@ -0,0 +1,19 @@
+import json
+from datetime import datetime
+from urllib.parse import urlunparse
+
+def app(environ, start_response):
+    time = datetime.now().strftime("%H:%M:%S")
+    url = urlunparse(("http", #scheme
+                     environ["SERVER_NAME"] + ':' + environ["SERVER_PORT"], #netloc
+                     environ["PATH_INFO"], #path
+                     "", #params
+                     environ["QUERY_STRING"], #query
+                     "" #fragment
+                    ))
+    raw_data = json.dumps(dict(time=time, url=url), indent=4).encode("utf-8")
+    start_response("200 OK",
+                   [("Content-Type", "text/plain"),
+                    ("Content-Length", str(len(raw_data))),
+                   ])
+    return iter([raw_data])
